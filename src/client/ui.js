88b915055fa0af5f100e981ca94a15884ffb6ff1
TriHard Studios <trihard.studios@outlook.com>
2021-02-04 22:22:52 -0700

    Worked on UI

--- src/client/ui.js
+++ src/client/ui.js
@@ -1,6 +1,9 @@
 
 
 function initializeUi(_resJson){
+    document.getElementById("userInfo_firstName").innerText = _resJson.firstName;
+    document.getElementById("userInfo_position").innerText = _resJson.position;
+    document.getElementById("nav_companyName").innerText = "Real Company LLC";
 
 
 
