9b431faf6fc10a630dadb907fbb4b4634c8f8af8
Gregory Bell <gregory@HackBook-Pro.local>
2021-02-10 14:56:34 -0700

    Created Stub functions for timeclock

--- src/server/rest/functionMap.mjs
+++ src/server/rest/functionMap.mjs
@@ -1,10 +1,10 @@
 import {fm_auth_manager} from "./auth/auth.mjs";
-
+import {fm_timeclock_punch} from "./timeclock/timeclock.mjs"
 
 let functionMap = new Map();
 
 functionMap.set("auth_manager_func", fm_auth_manager);
-
+functionMap.set("timeclock_punch_func", fm_timeclock_punch)
 
 
 
---
+++ src/server/rest/timeclock/timeclock.mjs
@@ -0,0 +1,17 @@
+
+
+
+function timeclock_punch(req, res){
+
+}
+
+function timeclock_overrideEmployee(req, res){
+
+}
+
+
+function timeclock_calcEmployee(req, res){
+
+}
+
+export {timeclock_punch as fm_timeclock_punch}
