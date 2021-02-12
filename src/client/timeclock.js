77c39cd677b518b15ef5b60bc4f749c9337dd4cc
TriHard Studios <trihard.studios@outlook.com>
2021-02-11 22:54:52 -0700

    Worked on timeclock UI

--- src/client/timeclock.js
+++ src/client/timeclock.js
@@ -14,6 +14,31 @@ function endLunch(e){
 
 }
 
+function currentTime(){
+    if(document.getElementById("main_timeClock_body").classList.contains("app_hidden")){
+        setTimeout(currentTime, 100);
+        return;
+    }
+    if(document.getElementById("main_timeClock_body").classList.contains("feature_disabled")){
+        return;
+    }
+    const cur = new Date();
+    //this gets the current time and formats it as hh:mm:ss
+    let hours = cur.getHours().toString();
+    hours = hours.length === 1 ? 0+hours : hours;
+    let minutes = cur.getMinutes().toString();
+    minutes = minutes.length === 1 ? 0+minutes : minutes;
+    let secs = cur.getSeconds().toString();
+    secs = secs.length === 1 ? 0+secs : secs;
+    const time = hours + ':' + minutes + ':' + secs;
+
+    document.getElementById("tc_currentTime").innerText = time;
+
+    setTimeout(currentTime, 1000);
+
+}
+currentTime();
+
 function linkToDom(){
     document.getElementById("tc_shiftStart_button").addEventListener("click", startShift);
     document.getElementById("tc_shiftEnd_button").addEventListener("click", endShift);
--- src/client/ui.js
+++ src/client/ui.js
@@ -12,17 +12,54 @@ function initializeUi(_resJson){
 function processRules(_rules){
     if(_rules.allowedPages.overview_allowed){
         document.getElementById("side_dashBoard_button").classList.remove("feature_disabled");
+        document.getElementById("main_dashBoard_body").classList.remove("feature_disabled");
+
+        document.getElementById("side_dashBoard_button").addEventListener('click',
+        (e)=>{
+                    showPage( document.getElementById("side_dashBoard_button"), document.getElementById("main_dashBoard_body"));
+                });
         //link function
     }
     if(_rules.allowedPages.time_clock_allowed){
         document.getElementById("side_timeClock_button").classList.remove("feature_disabled");
         document.getElementById("main_timeClock_body").classList.remove("feature_disabled");
+        document.getElementById("side_timeClock_button").addEventListener('click',
+            (e)=>{
+                showPage(document.getElementById("side_timeClock_button"), document.getElementById("main_timeClock_body"));
+            });
         TC_linkToDom();
     }
     if(_rules.allowedPages.employee_manager_allowed){
         document.getElementById("side_employeeManager_button").classList.remove("feature_disabled");
         //link function
     }
+}
+
+
+function hideAll(){
+    {
+        let domElement = document.getElementById("main_content");
+        for (let i = 0; i < domElement.childElementCount; i++) {
+            if (!domElement.children[i].classList.contains("feature_disabled"))
+                domElement.children[i].classList.add("app_hidden");
+        }
+    }
+    {
+        let domElement = document.getElementById("side_bar");
+        for (let i = 0; i < domElement.childElementCount; i++) {
+            if (!domElement.children[i].classList.contains("feature_disabled"))
+                domElement.children[i].classList.remove("button_active");
+        }
+    }
+}
+
+function showPage(_buttonDomElement, _pageDomElement){
+    hideAll();
+    if(!_pageDomElement.classList.contains("feature_disabled"))
+        _pageDomElement.classList.remove("app_hidden");
+    if(!_buttonDomElement.classList.contains("feature_disabled"))
+        _buttonDomElement.classList.add("button_active");
+
 }
 
 
