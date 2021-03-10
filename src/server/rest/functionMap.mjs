cdc8abc31ce38b0f12aa64a7b7c52848e98e8685
Gregory Bell <gregory@HackBook-Pro.local>
2021-03-10 11:34:41 -0700

    Made all functions follow same naming scheme

--- src/server/rest/functionMap.mjs
+++ src/server/rest/functionMap.mjs
@@ -8,7 +8,7 @@ let functionMap = new Map();
 functionMap.set("auth_manager_func", fm_auth_manager);
 functionMap.set("timeclock_punch_func", fm_timeclock_punch)
 functionMap.set("timeclock_current_func", fm_timeclockCurrent)
-functionMap.set("generate_session_ID_func", fm_generate_session_ID);
+functionMap.set("auth_generateSessionID_func", fm_generate_session_ID);
 functionMap.set("emp_getEmployees_func", fm_getEmployees);
 functionMap.set("emp_setEmployees_func", fm_setEmployees);
 functionMap.set("store_info_func", fm_systemStoreInfo);
